5d03c9d0e9c9cc4b9381500ff47c9af979e3e868
Aske Johansen <37876827+Krapylet@users.noreply.github.com>
2023-04-05 20:31:36 +0200

    Custom lighting now takes shadows from main light

--- myth-office/Assets/Shaders/CustomLighting.hlsl
+++ myth-office/Assets/Shaders/CustomLighting.hlsl
@@ -1,10 +1,20 @@
 #ifndef CUSTOM_LIGHTING
 #define CUSTOM_LIGHTING
 
+
+#ifndef SHADERGRAPH_PREVIEW
+#include "Packages/com.unity.render-pipelines.universal/Editor/ShaderGraph/Includes/ShaderPass.hlsl"
+#if (SHADERPASS != SHADERPASS_FORWARD)
+#undef REQUIRES_VERTEX_SHADOW_COORD_INTERPOLATOR
+#endif
+#endif
+
+
 struct CustomLightingData {
 	float3 normalWS; // normal of the fragment in world space
 	float3 viewDirection; // Direction from the camera to the fragment
-
+	float3 position; // Fragment world position
+	float4 shadowCoord; // coordnate used to look up in shadow map?
 
 	float3 albedo; // Base RGB color with no light influence, eg. Sampled color from texture. 
 	float smoothness; // stength of specular highlight
@@ -22,8 +32,8 @@ float GetSmoothnessPower(float rawSmoothness) {
 	// compute the diffuse light on the fragment
 	float3 CustomLightHandling(CustomLightingData d, Light light) {
 
-	// the light has an RGB color
-	float3 radiance = light.color; 
+	// the light has an RGB color. Darken that based on the shadow of other objects.
+	float3 radiance = light.color * light.shadowAttenuation; 
 
 	// Get the light strength based on the angle between the normal and the light direction. Clamped between 0-1
 	float diffuseStrength = saturate(dot(d.normalWS, light.direction));
@@ -45,11 +55,11 @@ float3 CalculateCustomLighting(CustomLightingData d) {
 		// assume light direction and calculate based on that.
 		float3 lightDir = float3(0.5, 0.5, 0);
 		float intensity = saturate(dot(d.normalWS, lightDir)) +
-			pow(saturate(dot(d.normalWS, normalize(d.viewDirection + lightDir))), GetSmoothnessPower(d.smoothness);
+			pow(saturate(dot(d.normalWS, normalize(d.viewDirection + lightDir))), GetSmoothnessPower(d.smoothness)); 
 		return d.albedo * intensity;	
 	#else
 		// Returns the single main light in the scene. Must be a directional light?
-		Light mainLight = GetMainLight();
+		Light mainLight = GetMainLight(d.shadowCoord, d.position, 1);
 
 		// start with a black pixel because no light has hit it yet.
 		float3 color = 0;
@@ -64,13 +74,26 @@ float3 CalculateCustomLighting(CustomLightingData d) {
 
 // Wrapper function called by shader graph. Output is provided through out variables
 // _float suffix specifies precision level on GPU
-void CalculateCustomLighting_float(float3 Albedo, float3 Normal, float3 ViewDirection, float Smoothness,
+void CalculateCustomLighting_float(float3 Albedo, float3 Normal, float3 ViewDirection, float Smoothness, float3 Position,
 	out float3 Color) {
 	CustomLightingData d;
 	d.albedo = Albedo;
 	d.normalWS = Normal;
 	d.viewDirection = ViewDirection;
 	d.smoothness = Smoothness;
+	d.position = Position;
+
+	// Calculate the shadow coord based on the fragment position.
+#ifdef SHADERGRAPH_PREVIEW
+	d.shadowCoord = 0;
+#else
+	float4 positionCS = TransformWorldToHClip(Position);
+	#if SHADOWS_SCREEN
+	d.shadowCoord = ComputeScreenPos(PositionCS);
+	#else
+	d.shadowCoord = TransformWorldToShadowCoord(Position);
+	#endif
+#endif
 
 	Color = CalculateCustomLighting(d);
 }
