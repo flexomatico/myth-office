0d100b3dd0b767c28bff0a889430f3588aee008b
Aske Johansen <37876827+Krapylet@users.noreply.github.com>
2023-04-05 20:09:47 +0200

    Added specular highlights to custom lighting

--- myth-office/Assets/Shaders/CustomLighting.hlsl
+++ myth-office/Assets/Shaders/CustomLighting.hlsl
@@ -3,9 +3,18 @@
 
 struct CustomLightingData {
 	float3 normalWS; // normal of the fragment in world space
+	float3 viewDirection; // Direction from the camera to the fragment
+
 
 	float3 albedo; // Base RGB color with no light influence, eg. Sampled color from texture. 
+	float smoothness; // stength of specular highlight
+
 };
+
+// translate 0-1 smoothness to an exponent, reducing the highlight as smothness grows
+float GetSmoothnessPower(float rawSmoothness) {
+	return exp2(10 * rawSmoothness + 1);
+}
 
 
 // Light class is not avalible in preview window
@@ -14,16 +23,17 @@ struct CustomLightingData {
 	float3 CustomLightHandling(CustomLightingData d, Light light) {
 
 	// the light has an RGB color
-	float3 radiance = light.color;
+	float3 radiance = light.color; 
 
 	// Get the light strength based on the angle between the normal and the light direction. Clamped between 0-1
-	float diffuse = saturate(dot(d.normalWS, light.direction));
-
+	float diffuseStrength = saturate(dot(d.normalWS, light.direction));
+	float specularBase = saturate(dot(d.normalWS, normalize(light.direction + d.viewDirection)));
+	float specularStrength = diffuseStrength * pow(specularBase, GetSmoothnessPower(d.smoothness));
 	// combine the color and strength of the light hitting the fragment.
-	float3 lightInfluence = radiance * diffuse;
+	float3 lightStrength = radiance * (diffuseStrength + specularStrength); 
 
-	// Use the influence to light up the base texture color sample
-	float3 color = d.albedo * lightInfluence;
+	// Use the strength of the light to light up the base texture color sample
+	float3 color = d.albedo * lightStrength;
 
 	return color;
 	}
@@ -34,7 +44,8 @@ float3 CalculateCustomLighting(CustomLightingData d) {
 	#ifdef SHADERGRAPH_PREVIEW
 		// assume light direction and calculate based on that.
 		float3 lightDir = float3(0.5, 0.5, 0);
-		float intensity = saturate(dot(d.normalWS, lightDir));
+		float intensity = saturate(dot(d.normalWS, lightDir)) +
+			pow(saturate(dot(d.normalWS, normalize(d.viewDirection + lightDir))), GetSmoothnessPower(d.smoothness);
 		return d.albedo * intensity;	
 	#else
 		// Returns the single main light in the scene. Must be a directional light?
@@ -53,11 +64,13 @@ float3 CalculateCustomLighting(CustomLightingData d) {
 
 // Wrapper function called by shader graph. Output is provided through out variables
 // _float suffix specifies precision level on GPU
-void CalculateCustomLighting_float(float3 Albedo, float3 Normal,
+void CalculateCustomLighting_float(float3 Albedo, float3 Normal, float3 ViewDirection, float Smoothness,
 	out float3 Color) {
 	CustomLightingData d;
 	d.albedo = Albedo;
 	d.normalWS = Normal;
+	d.viewDirection = ViewDirection;
+	d.smoothness = Smoothness;
 
 	Color = CalculateCustomLighting(d);
 }
