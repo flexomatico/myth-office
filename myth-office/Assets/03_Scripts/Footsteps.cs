6302ab606ca3c89705839fda804d87eee5955b56
femo <femo@itu.dk>
2023-05-23 18:22:49 +0200

    Disable footsteps in menu and dialogue

--- myth-office/Assets/03_Scripts/Footsteps.cs
+++ myth-office/Assets/03_Scripts/Footsteps.cs
@@ -1,26 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Footsteps : MonoBehaviour
 {
 
-public AudioSource footstepsSound; 
-
-
+    public AudioSource footstepsSound;
+    public PlayerInput playerInput;
     void Update()
     {
+        if (playerInput.currentActionMap.name != "Player")
+        {
+            return;
+        }
+        
         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D))
-
-{ 
-footstepsSound.enabled = true; 
-}
-
-else 
-{ 
-footstepsSound.enabled = false;
-}
-
+        {
+            footstepsSound.enabled = true; 
+        }
+        else 
+        { 
+            footstepsSound.enabled = false;
+        }
     }
 
 }
